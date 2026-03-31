074229d8d1618c20957d013d42a5ec1c9b9b1d64
babistranger <gabriellemab@gmail.com>
2026-03-31 17:14:50 -0300

    criando big boss_v2

--- Big_Boss.gd
+++ Big_Boss.gd
@@ -5,7 +5,6 @@ enum State {
 	fly
 }
 
-
 @onready var wall_detector: RayCast2D = $WallDetector
 @onready var ground_detector: RayCast2D = $GroundDetector
 
@@ -15,7 +14,21 @@ var altitude: float = 50.0
 var speed: float = 80.0
 var direction: int = 1
 
-func move():
+func go_to_fly_state():                            #estado de transição para o inimigo patrulhar
+	status = Big_BossState.fly
+	anima.play("fly")     
+
+func _physics_process(delta):
+	move(delta)
+    match state:
+		Big_BossState.fly:                 #O status do Big boss que al[em das funções do enemy, voa]
+			fly_state(delta)
+	move_and_slide()
+
+func fly_state(delta):                     #função de voar
+	move(delta)
+
+    func move():
 	velocity.x = speed * direction
 	
 	velocity.y = -altitude            #voo em altura constante
@@ -27,14 +40,4 @@ func turn_around(delta):
 	direction *= -1
 	
 	wall_detector.target_position.x *= -1         #inverter os detectores 
-	ground_detector.target_position.x *= -1         
-
-func _physics_process(delta):
-	move(delta)
-    match state:
-		Big_BossState.fly:
-			fly_state(delta)
-	move_and_slide()
-
-func fly_state(delta):
-	move(delta)
+	ground_detector.target_position.x *= -1    
